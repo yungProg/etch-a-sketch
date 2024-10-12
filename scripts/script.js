cb17877dd03db7b6582fe9e0355a1d4cd05d4cd0
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-10-12 12:47:23 +0000

    Generate random background color upon hover.
    Background color opacity increases by 0.1 upon each hover.

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,35 @@ const container = document.querySelector(".container");
 const createNewSheet = document.querySelector(".create-new-sheet-btn");
 
 function makeGrids(numberOfColumns = 16) {
-    let numberOfGrids = numberOfColumns**2
+    let numberOfGrids = numberOfColumns**2;
     for(let i = 0; i < numberOfGrids; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid");
         cell.style.flexBasis = `${960 / numberOfColumns}px`;
+        cell.setAttribute("gridOpacity", 0);
         container.appendChild(cell);
     }
-    console.log(container);
-    
+    console.log(container.style);
 }
 
-function generateRandomColor() {
-    const randomNumber1 = Math.floor(Math.random() * 256) // generate random number between 0 - 255
-    const randomNumber2 = Math.floor(Math.random() * 256) // generate random number between 0 - 255
-    const randomNumber3 = Math.floor(Math.random() * 256) // generate random number between 0 - 255
-    const randomColor = `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`
-    return randomColor
+function generateRandomColor(opacity) {
+    const randomNumber1 = Math.floor(Math.random() * 256); // generate random number between 0 - 255
+    const randomNumber2 = Math.floor(Math.random() * 256); // generate random number between 0 - 255
+    const randomNumber3 = Math.floor(Math.random() * 256); // generate random number between 0 - 255
+    const randomColor = `rgba(${randomNumber1}, ${randomNumber2}, ${randomNumber3}, ${opacity})`;
+    return randomColor;
 }
 
 container.addEventListener("mouseover", (event) => {
     target = event.target;
-    target.style.backgroundColor = generateRandomColor()
+    let opacity = Number(target.getAttribute("gridOpacity")) + 0.1;
+    target.setAttribute("gridOpacity", opacity)    
+    target.style.backgroundColor = generateRandomColor(opacity);
 })
 
 container.addEventListener("mouseout", (event) => {
     target = event.target;
-    target.style.backgroundColor = "inherit"
+    target.style.backgroundColor = "inherit";
 })
 
 createNewSheet.addEventListener("click", (e) => {
@@ -39,10 +41,10 @@ createNewSheet.addEventListener("click", (e) => {
         convertToNumber < 1 ||
         isNaN(convertToNumber)
     ) {
-        alert("Number of columns should be between 1 - 100")
-        return
+        alert("Number of columns should be between 1 - 100");
+        return;
     }
-    container.innerHTML = ""
-    makeGrids(convertToNumber)    
+    container.innerHTML = "";
+    makeGrids(convertToNumber);
 })
-makeGrids()
+makeGrids();
